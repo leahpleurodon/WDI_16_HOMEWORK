d830aa84f0d2ab46c741b58fc5e3d23faf75c557
Leah Eramo <leah.eramo@rea-group.com>
2018-08-06 16:52:13 +1000

    code to generate pixels

--- Leah/wk08/1-mon/app.js
+++ Leah/wk08/1-mon/app.js
@@ -1,8 +1,23 @@
 console.log("hello");
 let hexColor = "#000"
 
-$(".color-picker").on("submit",()=>{
-    event.preventDefault();
+
+
+
+const changeColor = () => {
     hexColor = $(".hex-color").val() || "#000"
     $(".color-indicator").css('background-color', hexColor);
+};
+
+const addPixels = () => {
+    for (let i = 0; i < 100; i++) {
+        $("main").append("<div class='pixel'>&nbsp</div>")
+    };
+};
+
+
+$(".color-picker").on("submit",()=>{
+    event.preventDefault();
 })
+
+addPixels();
